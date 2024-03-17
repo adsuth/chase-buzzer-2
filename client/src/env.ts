1a49cdbcf0a90665723a6001fd26d461349a66a0
sutson <aberdeenrules@hotmail.com>
2024-03-17 18:37:09 +0000

    change localhost

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,4 +1,4 @@
 const IS_PROD       = false
 const PORT          = 4000
 
-export const SOCKET_ORIGIN = IS_PROD ? "" : `http://localhost:${PORT}`
+export const SOCKET_ORIGIN = IS_PROD ? "" : `http://127.0.0.1:${PORT}`
